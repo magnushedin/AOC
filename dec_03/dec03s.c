7d718d783dbe9f794e92f61ae2dea8ad19bd0a35
Magnus Hedin <magnus@lmhedin.se>
2018-12-03 08:35:44 +0100

    Adding first part (not working) of Dec_03

---
+++ dec_03/dec03s.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_X 10
+#define MAX_Y 10
+
+int main(int argc, char* argv[]) {
+    FILE *fp;
+    char buff[30];
+    char subbuff[30];
+    int inputArray[MAX_X][MAX_Y] = { 0 };
+    int nbrOfInput = 0;
+
+    int x = 0, y = 0, dx = 0, dy = 0;
+    char *startPoint, *endPoint, *number;
+    int answer = 0;
+
+    char s[1014];
+
+    fp = fopen("small", "r");
+
+    for (int i=0; i<MAX_X; i++) {
+        for (int j=0; j<MAX_Y; j++) {
+            inputArray[i][j] = 0;
+        }
+    }
+
+    // Read input from file and store in array
+    while(fgets(buff, 30, fp) != NULL) {
+        nbrOfInput++;
+        printf("%d - %s",nbrOfInput, buff);
+
+        // X value
+        startPoint = strchr(buff, '@');
+        endPoint = strchr(buff, ',');
+        if (startPoint != NULL) {
+            memcpy(subbuff, startPoint+2, endPoint-startPoint-2);
+            subbuff[endPoint-startPoint-1] = '\0';
+            x = atoi(subbuff);
+            printf("x = %d\n", x);
+        }
+
+        // Y value
+        startPoint = strchr(buff, ',');
+        endPoint = strchr(buff, ':');
+        if (startPoint != NULL) {
+            memcpy(subbuff, startPoint+1, endPoint-startPoint-1);
+            subbuff[endPoint-startPoint-1] = '\0';
+            y = atoi(subbuff);
+            printf("y = %d\n", y);
+        }
+
+        // dx value
+        startPoint = strchr(buff, ':');
+        endPoint = strchr(buff, 'x');
+        if (startPoint != NULL) {
+            memcpy(subbuff, startPoint+2, endPoint-startPoint-2);
+            subbuff[endPoint-startPoint-2] = '\0';
+            dx = atoi(subbuff);
+            printf("dx = %d\n", dx);
+            //printf("dx = %s\n", subbuff);
+        }
+
+        // dy value
+        startPoint = strchr(buff, 'x');
+        if (startPoint != NULL) {
+            dy = atoi(startPoint+1);
+            printf("dy = %d\n", dy);
+            //printf("dy = %s\n", startPoint+1);
+        }
+        
+        
+        for (int i=x; i<=dx+x-1; i++) {
+            for (int j=y; j<=dy+y-1; j++) {
+                inputArray[i][j]++;
+                //printf("%d", inputArray[i][j]);
+            }
+            //printf("\n");
+        }
+
+    }
+
+    // The answer
+    for (int i=0; i<MAX_X; i++) {
+        for (int j=0; j<MAX_Y; j++) {
+            if (inputArray[i][j] > 1) {
+                answer++;
+            }
+        }
+    }
+
+    /*for (int i=0; i<MAX_X; i++) {
+        for (int j=0; j<MAX_Y; j++) {
+            printf("%d", inputArray[i][j]);
+        }
+        printf("\n");
+    }*/
+
+    printf("The answer is: %d\n", answer);
+
+}
