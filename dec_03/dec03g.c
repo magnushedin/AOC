60430bbcc3b329eb3a285a2e8055438cde7a1a1b
Magnus Hedin <magnus.hedin.external@veoneer.com>
2018-12-04 10:29:57 +0100

    Dec 03 - Adding gold assignment

--- dec_03/dec03g.c
+++ dec_03/dec03g.c
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]) {
     int x = 0, y = 0, dx = 0, dy = 0;
     char *startPoint, *endPoint, *number;
     int answer = 0;
+    int bto = 0;
 
     char s[1014];
 
@@ -78,29 +79,81 @@ int main(int argc, char* argv[]) {
             }
             //printf("\n");
         }
-
-        if (nbrOfInput < 10) {
-            printf("%d;%d;%d;%d;", x, y, dx, dy);
-            printf("%s", buff);
-        }
     }
+
+    fclose(fp);
+
+    fp = fopen("input", "r");
 
     // The answer
-    for (int i=0; i<MAX_X; i++) {
-        for (int j=0; j<MAX_Y; j++) {
-            if (inputArray[i][j] > 1) {
-                answer++;
-            }
+    nbrOfInput = 0;
+    while (fgets(buff, 30, fp) != NULL)
+    {
+        nbrOfInput++;
+
+        // X value
+        startPoint = strchr(buff, '@');
+        endPoint = strchr(buff, ',');
+        if (startPoint != NULL)
+        {
+            memcpy(subbuff, startPoint + 2, endPoint - startPoint - 2);
+            subbuff[endPoint - startPoint - 2] = '\0';
+            x = atoi(subbuff);
+            //printf("x = %d\n", x);
         }
+
+        // Y value
+        startPoint = strchr(buff, ',');
+        endPoint = strchr(buff, ':');
+        if (startPoint != NULL)
+        {
+            memcpy(subbuff, startPoint + 1, endPoint - startPoint - 1);
+            subbuff[endPoint - startPoint - 1] = '\0';
+            y = atoi(subbuff);
+            //printf("y = %d\n", y);
+        }
+
+        // dx value
+        startPoint = strchr(buff, ':');
+        endPoint = strchr(buff, 'x');
+        if (startPoint != NULL)
+        {
+            memcpy(subbuff, startPoint + 2, endPoint - startPoint - 2);
+            subbuff[endPoint - startPoint - 2] = '\0';
+            dx = atoi(subbuff);
+            //printf("dx = %d\n", dx);
+            //printf("dx = %s\n", subbuff);
+        }
+
+        // dy value
+        startPoint = strchr(buff, 'x');
+        if (startPoint != NULL)
+        {
+            dy = atoi(startPoint + 1);
+            //printf("dy = %d\n", dy);
+            //printf("dy = %s\n", startPoint+1);
+        }
+
+        for (int i = x; i <= dx + x - 1; i++)
+        {
+            for (int j = y; j <= dy + y - 1; j++)
+            {
+                if ((inputArray[i][j] != 1) && (!bto)) {
+                    bto = 1;
+                };
+                //printf("%d", inputArray[i][j]);
+            }
+            //printf("\n");
+        }
+
+        if (!bto) {
+            answer = nbrOfInput;
+            printf("%s\n", buff);
+        }
+        bto = 0;
     }
 
-    // Print the map
-    for (int i=0; i<MAX_X; i++) {
-        for (int j=0; j<MAX_Y; j++) {
-            printf("%d", inputArray[i][j]);
-        }
-        printf("\n");
-    }
+    fclose(fp);
 
     printf("The answer is: %d\n", answer);
 
