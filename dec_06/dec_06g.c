c2e5c6062ef185817a4d19dc37c55e32c0d246e5
Magnus Hedin <magnus@lmhedin.se>
2018-12-06 22:27:29 +0100

    Dec 06 - Full solution for silver and gold star!

--- dec_06/dec_06g.c
+++ dec_06/dec_06g.c
@@ -41,32 +41,21 @@ int calculate_dist(int x1, int y1, int x2, int y2) {
     return (abs(x1-x2) + abs(y1-y2));
 }
 
-int min_distance(int x, int y, coord* coords, int nbr_of_input) {
+int sum_distance(int x, int y, coord* coords, int nbr_of_input) {
     int min = INT_MAX;
     int min_i = -1;
     int dist = 0;
     int nbr_of_min = 0;
     
     for (int i=0; i<nbr_of_input; i++) {
-        dist = calculate_dist(x, y, coords[i].x, coords[i].y);
+        dist += calculate_dist(x, y, coords[i].x, coords[i].y);
+    }
 
-        if (min > dist) {
-            min_i = i;
-            min = dist;
-        }
-        //printf("id: %d, dist to (%d, %d): %d\n", i, coords[i].x, coords[i].y, calculate_dist(x, y, coords[i].x, coords[i].y));
-    }
-    for (int i=0;i<nbr_of_input; i++) {
-        if (min == calculate_dist(x, y, coords[i].x, coords[i].y)) {
-            nbr_of_min++;
-        }
-    }
-    // Return index of shortest distance
-    if (nbr_of_min > 1) {
-        return -1;
+    if (dist < 10000) {
+        return 1;
     }
     else {
-        return min_i;
+        return 0;
     }
 }
 
@@ -74,7 +63,7 @@ void calculate_map(int area_array[MAX_Y][MAX_X], coord* coords, int nbr_of_input
     for (int y=0; y<MAX_Y; y++){
         for (int x=0; x<MAX_X; x++) {
             //printf("map(%d, %d)\n", y, x);
-            area_array[y][x] = min_distance(x, y, coords, nbr_of_input);
+            area_array[y][x] = sum_distance(x, y, coords, nbr_of_input);
         }
     }
 
@@ -131,15 +120,16 @@ void print_map(int area_array[MAX_Y][MAX_X])
     }
 }
 
-void calculate_areas(int area_array[MAX_Y][MAX_X], int *aoa_array)
+int calculate_area(int area_array[MAX_Y][MAX_X])
 {
+    int area = 0;
     for (int y=0;y<MAX_Y;y++) {
         for (int x=0; x<MAX_X; x++) {
-            if (area_array[y][x] >= 0) {
-                aoa_array[area_array[y][x]]++;
-            }
+            area += area_array[y][x];
         }
     }
+
+    return area;
 }
 
 void print_aoa_array(int *aoa_array, int nbr_of_input)
@@ -166,17 +156,12 @@ int main(int argc, char* argv[]) {
     for (int i=0; i< nbr_of_input; i++) {
         //printf("%d: (%d, %d)\n", i, coords[i].x, coords[i].y);
     }
-
+    //printf("%d\n", nbr_of_input);
     calculate_map(area_array, coords, nbr_of_input);
     //print_map(area_array);
-    calculate_areas(area_array, aoa_array);
-    //print_aoa_array(aoa_array, nbr_of_input);
+    largest_area = calculate_area(area_array);
     
-    remove_infinite(area_array, aoa_array);
-    
-    //print_aoa_array(aoa_array, nbr_of_input);
-    
-    largest_area = find_largest_area(aoa_array, nbr_of_input);
+    //largest_area = find_largest_area(aoa_array, nbr_of_input);
 
     
 
