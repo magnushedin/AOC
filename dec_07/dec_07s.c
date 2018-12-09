8f25f2f255ad4401a0ba90d87b7e63112c01b39d
Magnus Hedin <magnus@lmhedin.se>
2018-12-09 10:42:47 +0100

    Dec 07 - Silver part working

--- dec_07/dec_07s.c
+++ dec_07/dec_07s.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define AB_LENGTH 6
-#define MAX_NODES 6
+#define AB_LENGTH 26
+#define MAX_NODES 26
 
 typedef struct node {
-    char before[AB_LENGTH];
-    char after[AB_LENGTH];
-    char name;
     int used;
+    char parent[AB_LENGTH];
+    char child[AB_LENGTH];
+    char name;
 }node;
 
 // Read input from file and store in array
@@ -18,21 +18,21 @@ void read_input(char* filename, node* node_array) {
     FILE *fp;
     char buff[16];
     char *token;
-    char before;
-    char after;
+    char tmp_parent;
+    char tmp_child;
 
     fp = fopen(filename, "r");
 
     // Initialize node_array
     for (int i=0; i<AB_LENGTH; i++) {
         node_array[i].name = 'A'+i;
-        node_array[i].used = 0;
+        node_array[i].used = 1;
         //printf("idx: %d: %c\n", i, node_array[i].name);
         for (int j=0; j<AB_LENGTH-1; j++) {
-            node_array[i].before[j] = '\0';
-            node_array[i].after[j] = '\0';
-            //strncpy(node_array[i].before[j], "\0", 1);
-            //strncpy(node_array[i].after[j], "\0", 1);
+            node_array[i].parent[j] = '\0';
+            node_array[i].child[j] = '\0';
+            //strncpy(node_array[i].parent[j], "\0", 1);
+            //strncpy(node_array[i].child[j], "\0", 1);
         }
     }
 
@@ -40,21 +40,23 @@ void read_input(char* filename, node* node_array) {
         // get the first token
 
         token = strtok(buff, ",");
-        before = *token;
+        tmp_parent = *token;
 
         token = strtok(NULL, "\n");
-        after = *token;
+        tmp_child = *token;
         
-        node_array[before-65].before[strlen(node_array[before-65].before)] = after;
-        node_array[after-65].after[strlen(node_array[after-65].after)] = before;
+        node_array[tmp_child-65].parent[strlen(node_array[tmp_child-65].parent)] = tmp_parent;
+        node_array[tmp_parent-65].child[strlen(node_array[tmp_parent-65].child)] = tmp_child;
+        node_array[tmp_parent-65].used = 0;
+        node_array[tmp_child-65].used = 0;
 
         /*
-        printf(" -- %c before %c --\n", before, after);
-        printf("before idx: %d, after idx: %d\n", before-65, after-65);
-        printf("%c.after: %s\n", node_array[before-65].name, node_array[before-65].before);
-        printf("%c.before: %s\n", node_array[before-65].name, node_array[before-65].after);
-        printf("%c.after: %s\n", node_array[after-65].name, node_array[after-65].before);
-        printf("%c.before: %s\n", node_array[after-65].name, node_array[after-65].after);
+        printf(" --parent: %c, child %c --\n", tmp_parent, tmp_child);
+        printf("parent idx: %d, child idx: %d\n", tmp_parent-65, tmp_child-65);
+        printf("%c.child: %s\n", node_array[tmp_parent-65].name, node_array[tmp_parent-65].child);
+        printf("%c.parent: %s\n", node_array[tmp_parent-65].name, node_array[tmp_parent-65].parent);
+        printf("%c.child: %s\n", node_array[tmp_child-65].name, node_array[tmp_child-65].child);
+        printf("%c.parent: %s\n", node_array[tmp_child-65].name, node_array[tmp_child-65].parent);
         */
     }
     fclose(fp);
@@ -63,25 +65,43 @@ void read_input(char* filename, node* node_array) {
 void print_node_array(node* node_array)
 {
     for (int i=0; i<MAX_NODES; i++) {
-        printf("%c - before: %s, after: %s\n", node_array[i].name, node_array[i].before, node_array[i].after);
+        printf("%c - parent: %s, child: %s, used: %d\n", node_array[i].name, node_array[i].parent, node_array[i].child, node_array[i].used);
     }
 
 }
 
 char find_next_node(node* node_array)
 {
-    int shortest_node = 0;
+    int shortest_node = -1;
 
+    //printf("shortest: %c --- %d\n", node_array[shortest_node].name, node_array[shortest_node].used);
     for (int i=0; i<AB_LENGTH; i++) {
-        if ((strlen(node_array[shortest_node].before) > strlen(node_array[i].before)) && (node_array[i].used == 0)) {
-            shortest_node = i;
+        //printf("shortest: %c --- %d\n", node_array[shortest_node].name, node_array[shortest_node].used);
+        if (shortest_node == -1) {
+            if (node_array[i].used != 1) {
+                shortest_node = i;
+            }
+        }
+        else {
+            if (strlen(node_array[shortest_node].parent) > strlen(node_array[i].parent)) {
+                if  (node_array[i].used != 1) {
+                    shortest_node = i;
+                    //printf("shortest: %c --- %d\n", node_array[shortest_node].name, node_array[shortest_node].used);
+                }
+            }
         }
     }
 
-    if (strlen(node_array[shortest_node].before) == 0) {
+    if (strlen(node_array[shortest_node].parent) == 0) {
         node_array[shortest_node].used = 1;
+        //printf("Will not use %c again\n", node_array[shortest_node].name);
     }
-    return node_array[shortest_node].name;
+    if (shortest_node != -1) {
+        return node_array[shortest_node].name;
+    }
+    else {
+        return '\0';
+    }
 }
 
 // Removes all instances of a character from the array by reading and writing in the same array.
@@ -94,7 +114,7 @@ void remove_char(char* char_array, char char_to_remove) {
             write++;
         }
         else {
-            printf("Removing: %c\n", char_to_remove);
+            //printf("Removing: %c\n", char_to_remove);
         }
     }
 }
@@ -102,8 +122,8 @@ void remove_char(char* char_array, char char_to_remove) {
 void remove_char_from_node(node* node_array, char char_to_remove)
 {
     for (int i=0; i<AB_LENGTH; i++) {
-        remove_char(node_array[i].before, char_to_remove);
-        //remove_char(node_array[i].after, char_to_remove);
+        remove_char(node_array[i].parent, char_to_remove);
+        //remove_char(node_array[i].child, char_to_remove);
     }
 }
 
@@ -111,20 +131,17 @@ int main(int argc, char* argv[]) {
     char buff[64];
     node node_array[MAX_NODES];
     char next_node;
-    int answer = 0;
+    char answer[AB_LENGTH] = { '\0' };
 
-    read_input("testinput", node_array);
-
-    print_node_array(node_array);
+    read_input("input", node_array);
 
     for (int i=0; i<AB_LENGTH; i++) {
+        //print_node_array(node_array);
         next_node = find_next_node(node_array);
-        printf("Next node: %c\n", next_node);
+        //printf("Next node: %c\n", next_node);
         remove_char_from_node(node_array, next_node);
-        print_node_array(node_array);
+        answer[i] = next_node;
     }
 
-    // find node that doesn't need do be after any other, alphabetically. remove from all other 
-
-    printf("Largest area: %d\n", answer);
+    printf("Answer: %s\n", answer);
 }
