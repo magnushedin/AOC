de112b9395102ec0672a71c095ce9c921752cc2c
Magnus Hedin <magnus@lmhedin.se>
2018-12-13 23:33:47 +0100

    Dec 10 - Full solution

--- dec_10/dec_10s.c
+++ dec_10/dec_10s.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define FILENAME "testinput"
-#define NODES 31
+#define FILENAME "input"
+#define NODES 369
 
 typedef struct node {
     int x;
     int y;
     int xv;
     int yv;
-}node;
+} node;
 
 // Read input from file and store in array
 int read_input(char* filename, node* node_array) {
@@ -70,23 +70,68 @@ void update_all_nodes(int nbr_of_nodes, node *node_array)
     }
 }
 
+int min_value(int nbr_of_nodes, node* node_array, char coord) {
+    int min_v = INT_MAX;
+    for (int i=0; i<nbr_of_nodes; i++) {
+        if (coord == 'x') {
+            if (min_v > node_array[i].x) {
+                min_v = node_array[i].x;
+            }
+        }
+        else {
+            if (min_v > node_array[i].y) {
+                min_v = node_array[i].y;
+            }
+        }
+    }
+    
+    return min_v;
+}
+
+int max_value(int nbr_of_nodes, node* node_array, char coord) {
+    int max_v = INT_MIN;
+    for (int i=0; i<nbr_of_nodes; i++) {
+        if (coord == 'x') {
+            if (max_v < node_array[i].x) {
+                max_v = node_array[i].x;
+            }
+        }
+        else {
+            if (max_v < node_array[i].y) {
+                max_v = node_array[i].y;
+            }
+        }
+    }
+    
+    return max_v;
+}
+
+char coord(int x, int y, node* node_array, int nbr_of_nodes) {
+    for (int i=0; i<nbr_of_nodes; i++) {
+        if ((node_array[i].x == x) && (node_array[i].y == y)) {
+            return '#';
+        }
+    }
+    return '.';
+}
+
 void print_map(int nbr_of_nodes, node *node_array)
 {
-    char str[50] = {'\0'};
+    int min_x, min_y, max_x, max_y;
 
-    for (int x=0; x<nbr_of_nodes; x++) {
-        for (int y=0; y<nbr_of_nodes; y++) {
-            if (x == node_array[x].y) {
-                str[node_array[y].x] = '#';
-            }
-            else {
-                str[node_array[y].x] = '.';
-            }
+    //printf("min x: %d, min y: %d\n", min_value(nbr_of_nodes, node_array, 'x'), min_value(nbr_of_nodes, node_array, 'y'));
+    //printf("max x: %d, max y: %d\n", max_value(nbr_of_nodes, node_array, 'x'), max_value(nbr_of_nodes, node_array, 'y'));
+
+    min_x = min_value(nbr_of_nodes, node_array, 'x');
+    max_x = max_value(nbr_of_nodes, node_array, 'x');
+    min_y = min_value(nbr_of_nodes, node_array, 'y');
+    max_y = max_value(nbr_of_nodes, node_array, 'y');
+    
+    for (int y=min_y; y<=max_y; y++) {
+        for (int x=min_x; x<=max_x; x++) {
+            printf("%c",coord(x,y,node_array,nbr_of_nodes));
         }
-        printf("%s\n", str);
-        for (int i=0; i<50; i++) {
-            str[i] = '\0';
-        }
+        printf("\n");
     }
 }
 
@@ -97,18 +142,23 @@ int main(int argc, char* argv[]) {
 
     nbr_of_nodes = read_input(FILENAME, node_array);
 
+    /*
     for (int i=0; i<nbr_of_nodes; i++){
         printf("(%d,%d) - (%d,%d)\n", node_array[i].x, node_array[i].y, node_array[i].xv, node_array[i].yv);
     }
+    */
+
+    //print_map(nbr_of_nodes, node_array);
 
     while (!all_have_neghbours(nbr_of_nodes, node_array)) {
         update_all_nodes(nbr_of_nodes, node_array);
         count++;
+        //print_map(nbr_of_nodes, node_array);
     }
 
     print_map(nbr_of_nodes, node_array);
 
-    printf("Number of nodes = %d\n", nbr_of_nodes);
+    //printf("Number of nodes = %d\n", nbr_of_nodes);
     printf("Count = %d\n", count);
 
 }
