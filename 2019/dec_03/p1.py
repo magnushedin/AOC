62250f761783e20279040e8361d5b0d38729edf2
Magnus Hedin <magnus@lmhedin.se>
2019-12-04 22:32:05 +0100

    AOC-2019: Change from list to dict for storing coordinates

--- 2019/dec_03/p1.py
+++ 2019/dec_03/p1.py
@@ -1,4 +1,4 @@
-input_file = open('input')
+input_file = open('test_input')
 
 lines = []
 line_coords = []
@@ -7,43 +7,49 @@ for line in input_file:
 	lines.append(line.rstrip().split(','))
 
 for line in lines:
-	coords = []
+	coords = dict()
 	x = 0
 	y = 0
 	for instruction in line:
 		direction = instruction[0]
 		steps = int(instruction[1:])
-		
+
 		if direction == 'U':
 			for step in range(steps):
 				y += 1
-				coords.append((x, y))
+				if not (x, y) in coords:
+					coords[(x, y)] = 0
 		elif direction == 'R':
 			for step in range(steps):
 				x += 1
-				coords.append((x,y))
+				if not (x, y) in coords:
+					coords[(x, y)] = 0
 		elif direction == 'D':
 			for step in range(steps):
 				y -= 1
-				coords.append((x,y))			
+				if not (x, y) in coords:
+					coords[(x, y)] = 0
 		elif direction == 'L':
 			for step in range(steps):
 				x -= 1
-				coords.append((x,y))
+				if not (x, y) in coords:
+					coords[(x, y)] = 0
 		else:
 			print('Error, invalid command: {}'.format(direction))
 
-	tmp_set = set(coords)
-	coords = list(tmp_set)
 	line_coords.append(coords)
 	#print(line_coords)
 
 line_1 = line_coords[0]
 line_2 = line_coords[1]
 
+print(line_1)
+print(line_2)
+
 shortest = 99999999999
 
 print("Done adding coordinates")
+print("Length line_1: {}, lenght line_2: {}".format(len(line_1), len(line_2)))
 
 for coord in line_1:
 	#print(coord)
