d34ec9d0b81709e3a8afd8a17b669ff2b20999cb
Magnus Hedin <magnus.hedin.external@veoneer.com>
2019-12-04 15:16:25 +0100

    AOC-2019: Adding part 2 of dec_03

--- 2019/dec_03/p2.py
+++ 2019/dec_03/p2.py
@@ -7,34 +7,53 @@ for line in input_file:
 	lines.append(line.rstrip().split(','))
 
 for line in lines:
-	coords = []
+	coords = dict()
 	x = 0
 	y = 0
+	dist = 0
 	for instruction in line:
 		direction = instruction[0]
 		steps = int(instruction[1:])
-		
+
 		if direction == 'U':
 			for step in range(steps):
 				y += 1
-				coords.append((x, y))
+				dist += 1
+				if not (x, y) in coords:
+					coords[(x, y)] = dist
+				else:
+					pass
+					#dist = coords[(x, y)]
 		elif direction == 'R':
 			for step in range(steps):
 				x += 1
-				coords.append((x,y))
+				dist += 1
+				if not (x, y) in coords:
+					coords[(x, y)] = dist
+				else:
+					pass
+					#dist = coords[(x, y)]
 		elif direction == 'D':
 			for step in range(steps):
 				y -= 1
-				coords.append((x,y))			
+				dist += 1
+				if not (x, y) in coords:
+					coords[(x, y)] = dist
+				else:
+					pass
+					#dist = coords[(x, y)]
 		elif direction == 'L':
 			for step in range(steps):
 				x -= 1
-				coords.append((x,y))
+				dist += 1
+				if not (x, y) in coords:
+					coords[(x, y)] = dist
+				else:
+					pass
+					#dist = coords[(x, y)]
 		else:
 			print('Error, invalid command: {}'.format(direction))
 
-	tmp_set = set(coords)
-	coords = list(tmp_set)
 	line_coords.append(coords)
 	#print(line_coords)
 
@@ -44,11 +63,12 @@ line_2 = line_coords[1]
 shortest = 99999999999
 
 print("Done adding coordinates")
+print("Length line_1: {}, lenght line_2: {}".format(len(line_1), len(line_2)))
 
 for coord in line_1:
 	#print(coord)
 	if coord in line_2:
-		tmp = abs(coord[0]) + abs(coord[1])
+		tmp = line_1[coord] + line_2[coord]
 		#print(tmp)
 		if tmp < shortest:
 			shortest = tmp
