3e05eb57dc640b992b9ab03f61852e97825410b5
Magnus Hedin <magnus@lmhedin.se>
2020-07-17 01:05:54 +0200

    AOC2019: Add instructions for print and read from input
    Used by dec_05 first task

--- 2019/ic/ic.py
+++ 2019/ic/ic.py
@@ -4,6 +4,7 @@ class Ic:
     """ IntCode Computer Class"""
 
     def __init__(self, file_name):
+        """ Constructor """
         input_file = open(file_name)
         file_content = input_file.read()
         self.data = file_content.split(',')
@@ -12,19 +13,20 @@ class Ic:
         self.program_length = len(self.data)
 
     def __tick_computer(self, steps):
-        """ Tick the computer by moving program counter ahead """
+        """ Tick the computer by moving program counter given steps ahead """
         if (self.p_c + steps) == self.program_length:
             raise Exception("End of program reached")
         else:
             self.p_c += steps
 
     def get_operation(self):
-        """ Returns next command """
+        """ Returns operation at current program pointer """
         instruction = self.data[self.p_c]
         #self.tick_computer(1)
         return instruction
 
     def get_value(self, position):
+        """ Get value at given position """
         return int(self.data[position])
 
     def write_value(self, position, value):
@@ -35,17 +37,17 @@ class Ic:
         """ Start the computer at given position """
         self.p_c = p_c
 
-        print("Computer statrted at: {}".format(p_c))
+        print("Computer started at: {}".format(p_c))
         while True:
             instruction = self.get_operation()
-            print("PC: {}".format(self.p_c))
-            print("..{}".format(self.data))
+            print("PC: {}, instruction: {}".format(self.p_c, instruction))
+            # print("..{}".format(self.data)) # for debugging
 
-            if instruction == 99:
+            if instruction == 99: # Terminate program
                 print("OP-Code 99 found, terminating")
-                print(self.data)
+                # print(self.data) # for debugging
                 break
-            elif instruction == 1:
+            elif instruction == 1: # Add
                 read_pos_1 = self.get_value(self.p_c + 1)
                 read_pos_2 = self.get_value(self.p_c + 2)
                 write_pos = self.get_value(self.p_c + 3)
@@ -56,7 +58,7 @@ class Ic:
 
                 self.write_value(write_pos, result_value)
                 self.__tick_computer(4)
-            elif instruction == 2:
+            elif instruction == 2: # multiply
                 read_pos_1 = self.get_value(self.p_c + 1)
                 read_pos_2 = self.get_value(self.p_c + 2)
                 write_pos = self.get_value(self.p_c + 3)
@@ -67,5 +69,16 @@ class Ic:
 
                 self.write_value(write_pos, result_value)
                 self.__tick_computer(4)
+            elif instruction == 3: # Read from input
+                print("input parameter: ", end='')
+                input_value = input()
+                write_pos = self.get_value(self.p_c + 1)
+                self.write_value(write_pos, input_value)
+                # print("{}, {}".format(write_pos, self.get_value(write_pos))) # for debugging
+                self.__tick_computer(2)
+            elif instruction == 4: # Print value at position
+                read_pos = self.get_value(self.p_c + 1)
+                read_value = self.get_value(read_pos)
+                print("Print instruction, pos: {}, value: {}".format(read_pos, read_value))
             else:
                 raise Exception("Invalid op-code: {}".format(instruction))
