e231c4764a479cb17c996aa8f2b4c43065101d2d
Magnus Hedin <magnus@lmhedin.se>
2020-07-17 16:02:45 +0200

    AOC2019: Add files for dec_05 problem two

---
+++ 2019/dec_05/p2.py
@@ -0,0 +1,14 @@
+""" Problem 5b - 2019 """
+
+import ic
+import sys
+
+# For debugger to find the input
+file_name = "./2019/dec_05/input_2"
+
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+
+my_ic = ic.Ic(file_name)
+my_ic.start_computer(0)
+
