76ddb9ce01bf8e1b57d9c6bf35313180f2866000
Magnus Hedin <magnus@lmhedin.se>
2020-07-18 23:23:18 +0200

    SOC2019: Update file for dec_05 second problem

--- 2019/dec_05/p2.py
+++ 2019/dec_05/p2.py
@@ -4,10 +4,10 @@ import ic
 import sys
 
 # For debugger to find the input
-file_name = "./2019/dec_05/input_2"
+file_name = "./2019/dec_05/input_large_example"
 
 if len(sys.argv) > 1:
     file_name = sys.argv[1]
 
-my_ic = ic.Ic(file_name, 0)
+my_ic = ic.Ic(file_name, 4)
 my_ic.start_computer(0)
