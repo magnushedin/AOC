a3230f8a7ebc53f4cbd86c79510bfa5e176ed39c
Magnus Hedin <magnus@lmhedin.se>
2024-12-11 01:00:20 +0100

    AOC-2024: Day 9 part 1

--- 2024/day_09/d9a.py
+++ 2024/day_09/d9a.py
@@ -1,6 +1,61 @@
-f = open('data_train', 'r')
+def compress(data):
+    length = len(data)
+    for i, d in enumerate(data):
+        if (i > length):
+            break
+        if d == '.':
+            length -= 1
+            tmp = data[length]
+            data[length] = '.'
+            while tmp == '.':
+                length -= 1
+                tmp = data[length]
+                data[length] = '.'
+            data[i] = tmp
+            # print(''.join(data))
+        # print(f'{i}, {d}: {length}')
 
-for line in f:
-    data = line
+def checksum(data):
+    sum = 0
+    for i,d in enumerate(data):
+        if d != '.':
+            sum += i * int(d)
+    return sum
 
-print(data)
+if __name__ == '__main__':
+    f = open('data.txt', 'r')
+
+    for line in f:
+        data = line.rstrip()
+
+    unpack = []
+    file = True
+
+    i = 0
+    for d in data:
+        # print(f'running: {d}')
+        if file:
+            file = False
+            length = int(d)
+            # print(f'{i}, {d}')
+            for l in range(length):
+                unpack.append(str(i))
+            i += 1
+        else:
+            file = True
+            length = int(d)
+            for l in range(length):
+                unpack.append('.')
+
+    print(data)
+    # print(''.join(unpack))
+
+
+    compress(unpack)
+    while unpack[-1] == '.':
+        unpack.pop()
+    print(''.join(unpack))
+
+    unpack.pop(len(unpack) -2)
+
+    print(checksum(unpack))
